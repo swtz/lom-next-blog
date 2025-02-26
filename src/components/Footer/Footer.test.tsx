b1322d32a2384e39620e8cbc2eece535314584aa
Leonardo Lüders <luderser@hotmail.com>
2025-02-26 15:55:59 -0300

    'Footer' component reused in the project.

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,44 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { Footer } from '.';
+
+describe('<Footer />', () => {
+  it('should render string passed to html prop as html', () => {
+    const { container } = renderTheme(
+      <Footer footerHtml={'<h1>Children</h1>'} />,
+    );
+    expect(
+      screen.getByRole('heading', { name: 'Children' }),
+    ).toBeInTheDocument();
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        text-align: center;
+        border-top: 0.1rem solid #DDDDDD;
+      }
+
+      .c0 a {
+        color: inherit;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+      }
+
+      .c1 {
+        font-size: 2.4rem;
+      }
+
+      <div>
+        <footer
+          class="c0"
+        >
+          <div
+            class="c1"
+          >
+            <h1>
+              Children
+            </h1>
+          </div>
+        </footer>
+      </div>
+    `);
+  });
+});
---
+++ src/components/MenuLink/MenuLink.test.tsx
@@ -0,0 +1,73 @@
+import { screen } from '@testing-library/react';
+import { MenuLink } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="https://localhost">link</MenuLink>);
+    expect(screen.getByRole('link', { name: 'link' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
+
+  it('should render a internal link', () => {
+    renderTheme(<MenuLink link="/something">link</MenuLink>);
+    expect(screen.getByRole('link', { name: 'link' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="https://localhost" newTab>
+        link
+      </MenuLink>,
+    );
+    expect(screen.getByRole('link', { name: 'link' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(
+      <MenuLink link="https://localhost">link</MenuLink>,
+    );
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        display: block;
+        font-size: 1.6rem;
+        padding: 1.6rem;
+        position: relative;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+      }
+
+      .c0::after {
+        content: '';
+        position: absolute;
+        bottom: 0.8rem;
+        left: 50%;
+        width: 0;
+        height: 0.2rem;
+        -webkit-transition: all 300ms ease-in-out;
+        transition: all 300ms ease-in-out;
+      }
+
+      .c0:hover::after {
+        left: 25%;
+        width: 50%;
+      }
+
+      <a
+        class="c0"
+        href="https://localhost"
+        target="_self"
+      >
+        link
+      </a>
+    `);
+  });
+});
