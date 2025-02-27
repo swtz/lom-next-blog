c898d8b1f46876fce716b89a957884935420a6a5
Leonardo Lüders <luderser@hotmail.com>
2025-02-27 14:33:57 -0300

    'mock.ts' file created and used in the 'HtmlContent/stories.ts' to set 'html' prop to do styles (with Styled-components) for 'HtmlContent' component.

--- src/components/HtmlContent/stories.tsx
+++ src/components/HtmlContent/stories.tsx
@@ -1,14 +1,12 @@
 import { Meta, StoryFn } from '@storybook/react/types-6-0';
 import { HtmlContent, HtmlContentProps } from '.';
+import mock from './mock';
 
 export default {
   title: 'HtmlContent',
   component: HtmlContent,
   args: {
-    html: `<b>Lorem ipsum, dolor sit amet consectetur
-    adipisicing elit. Ipsa beatae voluptate quod rem quisquam
-    id illo ea voluptatum quidem enim autem, voluptatem necessitatibus
-    vel, a veritatis debitis porro, nam eveniet?</b>`,
+    html: mock,
   },
 } as Meta;
 
