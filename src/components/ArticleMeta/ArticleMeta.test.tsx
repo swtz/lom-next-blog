d9b2b4ec37e8587c45ed103233122498c29f1278
Leonardo Lüders <luderser@hotmail.com>
2025-03-04 06:50:57 -0300

    Finished class 251 (ArticleMeta Component) | Achieved 100% test coverage for this component.

--- src/components/ArticleMeta/ArticleMeta.test.tsx
+++ src/components/ArticleMeta/ArticleMeta.test.tsx
@@ -1,15 +1,44 @@
 import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
-import { ArticleMeta, ArticleMetaProps } from '.';
+import { ArticleMeta } from '.';
 
-const props: ArticleMetaProps = {
-  title: 'any',
-};
+import mock from './mock';
 
 describe('<ArticleMeta />', () => {
-  it('should render', () => {
-    renderTheme(<ArticleMeta {...props} />);
+  it('should render author and category links', () => {
+    renderTheme(<ArticleMeta {...mock} />);
 
-    expect(screen.getByRole('heading', { name: 'Oi' })).toBeInTheDocument();
+    expect(
+      screen.getByRole('link', {
+        name: mock.author.data.attributes.displayName,
+      }),
+    ).toHaveAttribute('href', '/author/surfista');
+
+    expect(
+      screen.getByRole('link', {
+        name: mock.categories.data[0].attributes.displayName,
+      }),
+    ).toHaveAttribute('href', '/category/off-topic');
+
+    expect(
+      screen.getByRole('link', {
+        name: mock.categories.data[1].attributes.displayName,
+      }),
+    ).toHaveAttribute('href', '/category/tech');
+  });
+
+  it('should format date', () => {
+    renderTheme(<ArticleMeta {...mock} />);
+
+    expect(screen.getByText('12 de fev. de 2025')).toHaveAttribute(
+      'datetime',
+      mock.createdAt,
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<ArticleMeta {...mock} />);
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
