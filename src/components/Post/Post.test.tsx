28e744168c0f337254e358aca731ff8d4d2f39e1
Leonardo Lüders <luderser@hotmail.com>
2025-03-06 10:01:15 -0300

    Completed testing for the 'Post' component.

--- src/components/Post/Post.test.tsx
+++ src/components/Post/Post.test.tsx
@@ -13,12 +13,22 @@ const props: PostProps = {
 };
 
 describe('<Post />', () => {
-  it('should render', () => {
+  it('should render elements from the Header, ArticleHeader, and HtmlContent components inside Post.', () => {
     const spreadProps = { ...props };
     renderTheme(<Post {...spreadProps} />);
 
     expect(
       screen.getByRole('heading', { name: 'Blog do Arroz é vida!' }),
     ).toBeInTheDocument();
+
+    expect(screen.getByAltText(props.excerpt)).toBeInTheDocument();
+
+    expect(screen.getByRole('blockquote')).toBeInTheDocument();
+  });
+
+  it('should render elements from the Header, ArticleHeader, and HtmlContent components inside Post.', () => {
+    const spreadProps = { ...props };
+    const { container } = renderTheme(<Post {...spreadProps} />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
