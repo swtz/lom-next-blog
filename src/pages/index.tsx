cc1c74202a3d3c6f7ede9e0c67fe02e8d9823144
Leonardo Lüders <luderser@hotmail.com>
2025-03-22 11:30:12 -0300

    Finished class 274 (Home page with 'getStaticProps' (SSG of Next) | Google Drive: aula217.mp4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,43 @@
-import { useEffect } from 'react';
+import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import { PostsTemplate, PostsTemplateProps } from '../templates/PostsTemplate';
 import { loadPosts } from '../api/load-posts';
 
-export default function Index() {
-  useEffect(() => {
-    loadPosts().then((r) => console.log(r.setting, r.posts));
-  }, []);
-
-  return <h1>Hello, world!</h1>;
+export default function Index({ posts, setting }: PostsTemplateProps) {
+  return (
+    <>
+      <Head>
+        <title>{setting.data.attributes.blogName}</title>
+        <meta
+          name="description"
+          content={setting.data.attributes.blogDescription}
+        />
+      </Head>
+      <PostsTemplate posts={posts} setting={setting} />
+    </>
+  );
 }
+
+export const getStaticProps: GetStaticProps<PostsTemplateProps> = async () => {
+  let data = null;
+
+  try {
+    data = await loadPosts();
+  } catch (e) {
+    data = null;
+    console.log('Error: getStaticProps from Index component', e.message);
+  }
+
+  if (!data || !data.posts || !data.posts.data.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      posts: data.posts,
+      setting: data.setting,
+    },
+  };
+};
